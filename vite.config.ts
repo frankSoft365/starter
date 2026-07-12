520577626bb5b9e26ccbd3855706958f29c31d8e
frank <264430662@qq.com>
2026-07-12 11:02:13 +0800

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
@@ -14,6 +15,30 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset(), 'jotai-babel/preset'] }),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Aedium',
+        short_name: 'Aedium',
+        description: 'Share your story.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
