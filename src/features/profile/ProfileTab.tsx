99bf400c61de353052e9477e0afcd759479b3844
frank <264430662@qq.com>
2026-07-21 16:14:09 +0800

    feat: add username in profile page

--- src/features/profile/ProfileTab.tsx
+++ src/features/profile/ProfileTab.tsx
@@ -1,26 +1,31 @@
+import { useAtomValue } from "jotai";
 import MyArticleList from "./MyArticleList";
+import { userAtom } from "@/atoms/user";
 
 export default function ProfileTab() {
-
+    const user = useAtomValue(userAtom);
 
     return (
-        <div className="tabs tabs-lift w-full md:w-3xl">
-            <input type="radio" name="profile_tabs_5" className="tab" aria-label="Home" defaultChecked />
-            <div className="tab-content bg-base-100 border-base-300">
-                <MyArticleList />
+        <div className="w-full md:w-3xl">
+            <div className="w-full text-3xl md:text-4xl m-4 font-bold">{user?.username || 'Unknown username'}</div>
+            <div className="tabs tabs-lift w-full">
+                <input type="radio" name="profile_tabs_5" className="tab" aria-label="Home" defaultChecked />
+                <div className="tab-content bg-base-100 border-base-300">
+                    <MyArticleList />
+                </div>
+
+                <input type="radio" name="profile_tabs_5" className="tab" aria-label="Reposts" />
+                <div className="tab-content bg-base-100 border-base-300 p-6">Reposts</div>
+
+                <input type="radio" name="profile_tabs_5" className="tab" aria-label="Activity" />
+                <div className="tab-content bg-base-100 border-base-300 p-6">Activity</div>
+
+                <input type="radio" name="profile_tabs_5" className="tab" aria-label="Lists" />
+                <div className="tab-content bg-base-100 border-base-300 p-6">Lists</div>
+
+                <input type="radio" name="profile_tabs_5" className="tab" aria-label="About" />
+                <div className="tab-content bg-base-100 border-base-300 p-6">About</div>
             </div>
-
-            <input type="radio" name="profile_tabs_5" className="tab" aria-label="Reposts" />
-            <div className="tab-content bg-base-100 border-base-300 p-6">Reposts</div>
-
-            <input type="radio" name="profile_tabs_5" className="tab" aria-label="Activity" />
-            <div className="tab-content bg-base-100 border-base-300 p-6">Activity</div>
-
-            <input type="radio" name="profile_tabs_5" className="tab" aria-label="Lists" />
-            <div className="tab-content bg-base-100 border-base-300 p-6">Lists</div>
-
-            <input type="radio" name="profile_tabs_5" className="tab" aria-label="About" />
-            <div className="tab-content bg-base-100 border-base-300 p-6">About</div>
         </div>
     );
 }
