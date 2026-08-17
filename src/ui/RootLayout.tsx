d79824d578bc5a3f16f3200ac00c35f4f3219a88
frank <264430662@qq.com>
2026-08-17 19:54:33 +0800

    opt: remove menu in article edit page

--- src/ui/RootLayout.tsx
+++ src/ui/RootLayout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     {!isEditorRoute && !isArticleEditRoute && <Footer />}
                 </div>
                 <DrawerSide onNav={setDrawerOpen} />
-                {!isEditorRoute && <BottomMenu />}
+                {(!isEditorRoute && !isArticleEditRoute) && <BottomMenu />}
             </div>
         </>
     );
